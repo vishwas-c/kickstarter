1558ff4c60c832d4f8e6882837f72a11cfdb7cd5
Vishwas <vishwasblaze@gmail.com>
2019-04-19 19:32:37 +0530

    Arrays and Methods

--- jscrash/main.js
+++ jscrash/main.js
@@ -1,5 +1,5 @@
-// console.log("Console log displaying");
-// console.clear();
+console.log("Console log displaying");
+//console.clear();
 // console.error("Oops This is an error");
 // console.warn("Oops This is a warning");
 
@@ -27,3 +27,21 @@ console.log(name.toUpperCase());
 console.log(name.toLowerCase());
 console.log(name.substring(0,2)); //Vi
 console.log(name.split(' ')); // Converts to array
+
+//Arrays
+const numbers = new Array(1,2,3,4,5,6,7);
+console.log(numbers);
+
+// [] brackets does same way
+const arrays = [1,'apples','mango',true,null,6,7];
+arrays[6] = 'Vish';
+console.log(arrays);
+console.log(arrays[1]);
+
+arrays.push(true);
+arrays.unshift('strawberry');
+console.log(arrays);
+arrays.pop();
+console.log(arrays);
+console.log(Array.isArray(arrays));
+console.log(arrays.indexOf('Vish'));
