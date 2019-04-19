f8ebbb8cbc24dfdd4efefa64994251c4498bc3a8
Vishwas <vishwasblaze@gmail.com>
2019-04-19 17:39:27 +0530

    Datatypes

--- jscrash/main.js
+++ jscrash/main.js
@@ -2,3 +2,17 @@
 // console.clear();
 // console.error("Oops This is an error");
 // console.warn("Oops This is a warning");
+
+//Primitive Datatypes
+// String, Numbers, Boolean , null, undefined
+
+const name = 'Vishwas';
+const age = 23;
+const rating = 4.5;
+const isawesome = true;
+const x = null;
+const y = undefined;
+let z;
+
+console.log(typeof(name),typeof(age), typeof(rating),typeof(isawesome),typeof(x),typeof(y),typeof(z));
+
